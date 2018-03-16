d14a516e2da119830331ca2ef5550c8ff86f5f6a
ALMerrill <merrillman32@gmail.com>
2018-03-16 16:32:32 -0600

    added top champs for the summoner

--- public/script.js
+++ public/script.js
@@ -3,7 +3,7 @@ var app = new Vue({
   data: {
     addedName: '',
     // summoners: {},
-    summoner: {},
+    summoner: {topChamps: ["", "", ""]},
     uri: 'localhost',
     show: false,
   },
@@ -24,10 +24,23 @@ var app = new Vue({
       }).then(response => {
         let icon = response.data.icon;
         this.show = true;
-        elem = document.getElementById('image');
+        summIcon = document.getElementById('image');
         path = 'img/' + String(icon) + '.png';
-        if(typeof elem !== 'undefined' && elem !== null) {
-          document.getElementById('image').src = 'img/' + String(icon) + '.png';
+        if(typeof summIcon !== 'undefined' && summIcon !== null) {
+          // document.getElementById('image').src = 'img/' + String(icon) + '.png';
+          summIcon.src = "http://ddragon.leagueoflegends.com/cdn/6.24.1/img/profileicon/" + String(icon) + ".png";
+        }
+        champImage1 = document.getElementById('champImage');
+        if(typeof champImage1 !== 'undefined' && champImage1 !== null) {
+          champImage1.src = "http://ddragon.leagueoflegends.com/cdn/6.24.1/img/champion/" + this.summoner.topChamps[0] + ".png"
+        }
+        champImage2 = document.getElementById('champImage');
+        if(typeof champImage2 !== 'undefined' && champImage2 !== null) {
+          champImage2.src = "http://ddragon.leagueoflegends.com/cdn/6.24.1/img/champion/" + this.summoner.topChamps[0] + ".png"
+        }
+        champImage3 = document.getElementById('champImage');
+        if(typeof champImage3 !== 'undefined' && champImage3 !== null) {
+          champImage3.src = "http://ddragon.leagueoflegends.com/cdn/6.24.1/img/champion/" + this.summoner.topChamps[0] + ".png"
         }
         this.addedName = "";
         this.getSummoner();
--- summoners.js
+++ summoners.js
@@ -12,8 +12,9 @@ app.use("img", express.static(__dirname + '/img'));
 
 app.use(express.static('public'));
 
-let summoner = {};
+var summoner = {};
 let id = 0;
+var topChamps;
 let api_key = "RGAPI-13320ace-e222-4f7b-9334-f7e8b1297eb8";
 
 // request("https://na1.api.riotgames.com/lol/static-data/v3/profile-icons?api_key=RGAPI-b3619dbd-6b7a-4851-88d3-d531fbeead88", function(error, response, body) {
@@ -24,31 +25,70 @@ let api_key = "RGAPI-13320ace-e222-4f7b-9334-f7e8b1297eb8";
 //   }
 // });
 
-for(let i = 1449; i < 3000; i++) {
+// for(let i = 1449; i < 3000; i++) {
   
-  http.get("http://ddragon.leagueoflegends.com/cdn/6.24.1/img/profileicon/" + String(i) + ".png ", function(res) {
-    if (res.statusCode == 200) {
-      res.pipe(fs.createWriteStream("public/img/" + String(i) + ".png"));
-    }
-  });
-}
+//   http.get("http://ddragon.leagueoflegends.com/cdn/6.24.1/img/profileicon/" + String(i) + ".png", function(res) {
+//     if (res.statusCode == 200) {
+//       res.pipe(fs.createWriteStream("public/img/" + String(i) + ".png"));
+//     }
+//   });
+// }
 
 app.get('/api/summoner', (req, res) => {
   res.send(summoner);
 });
 
 app.post('/api/summoner', (req, res) => {
+  var infoSummoner;
+  var infoMastery;
+  var infoChamps;
   request('https://na1.api.riotgames.com/lol/summoner/v3/summoners/by-name/' + req.body.name + '?api_key=' + api_key, function (error, response, body) {
     if (!error && response.statusCode == 200) {
-      var info = JSON.parse(body);
+      infoSummoner = JSON.parse(body);
       id = id + 1;
-      summoner = {id:id, name:info.name, lvl:info.summonerLevel, icon:info.profileIconId};
-      // summoner = summoner;
-      res.send(summoner);
+      summoner = {id:id, name:infoSummoner.name, lvl:infoSummoner.summonerLevel, icon:infoSummoner.profileIconId};
     }
     else {
-      console.log(response.body);
+      console.log("Summoner: " + response.body);
     }
+    request('https://na1.api.riotgames.com/lol/champion-mastery/v3/champion-masteries/by-summoner/' + String(infoSummoner.id) + '?api_key=' + api_key, function(error, response, body) {
+      if(!error && response.statusCode == 200) {
+        infoMastery = JSON.parse(body);
+        topChamps = [infoMastery[0].championId, infoMastery[1].championId, infoMastery[2].championId];
+      }
+      else {
+        console.log("Mastery: " + response.body)
+      }
+      request('https://na1.api.riotgames.com/lol/static-data/v3/champions?api_key=' + api_key, function(error, response, body) {
+        if(!error && response.statusCode == 200) {
+          infoChamps = JSON.parse(body);
+          var topChampNames = [];
+          console.log('1');
+          for(let champ in infoChamps.data) {
+            if(infoChamps.data[champ].id == topChamps[0]) {
+              topChampNames.push(champ);
+            }
+          }
+          console.log('2');
+          for(let champ in infoChamps.data) {
+            if(infoChamps.data[champ].id == topChamps[1]) {
+              topChampNames.push(champ);
+            }
+          }
+          console.log('3');
+          for(let champ in infoChamps.data) {
+            if(infoChamps.data[champ].id == topChamps[2]) {
+              topChampNames.push(champ);
+            }
+          }
+          summoner.topChamps = topChampNames;
+          res.send(summoner);
+        }
+        else {
+          console.log("Champ: " + response.body);
+        }
+      });
+    });
   });
 });
 
